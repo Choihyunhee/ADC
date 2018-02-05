821a3f62e5b223fd540fc72824068ee55702537d
Supermario <35128397+Choihyunhee@users.noreply.github.com>
2018-02-05 06:45:28 +0000

    Add files via upload

---
+++ touch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <wiringARTIK.h>
+
+int inputPin =128;
+
+int setup(){
+        if(!digitalPinMode(inputPin,INPUT)) return -1;
+        return 0;
+}
+
+int main(void){
+        if(setup()==-1) exit(1);
+        while(1)
+        {
+                printf("SensorVal : %d\n",digitalRead(inputPin));
+                sleep(1);
+        }
+        return 0;
+}
+
